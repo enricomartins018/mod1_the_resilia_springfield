f1b38b7485077883c4e74fe6e10947a486f06ad6
enricomartins018 <enrico.martins018@gmail.com>
2022-04-22 18:42:49 -0300

    Finalização do projeto!

---
+++ personagem2/fases.js
@@ -0,0 +1,74 @@
+function fase1() {
+    var vidas = 0;
+    while (true) {
+        var option = (prompt("Digite a opção correta:").toUpperCase());
+
+        if (option == 'C') {
+            alert('Resposta correta!!! Parabéns!!!');
+            location.assign("./fase2.html")
+            break;
+        } else if (option == 'A' || option == 'B') {
+            if(vidas >= 1){
+                location.assign("./gameover1.html")
+                break;
+            }else{
+            alert('Se errar novamente, seu corpo será desovado na vila da folha.');
+            vidas++;
+            }
+
+        } else {
+            alert("Digite apenas A, B ou C")
+        }
+    }
+}
+
+function fase2() {
+    var vidas = 0;
+    while (true) {
+        var option = (prompt("Digite a opção correta:").toUpperCase());
+
+        if (option == 'C') {
+            alert('Resposta correta!!! Parabéns!!!');
+            location.assign("./fase3.html")
+            break;
+        } else if (option == 'A' || option == 'B') {
+            if(vidas >= 1){
+                location.assign("./gameover1.html")
+                break;
+            }else{
+            alert('Se errar novamente, seu corpo será desovado na vila da folha.');
+            vidas++;
+            }
+
+        } else {
+            alert("Digite apenas A, B ou C")
+        }
+    }
+}
+
+function fase3() {
+    var vidas = 0;
+    while (true) {
+        var option = (prompt("Resposta correta!!! Parabéns!!!").toUpperCase());
+
+        if (option == 'C') {
+            alert('Resposta correta!!! Parabéns!!!');
+            location.assign("./gamewin1.html")
+            break;
+        } else if (option == 'A' || option == 'B') {
+            if(vidas >= 1){
+                location.assign("./gameover1.html")
+                break;
+            }else{
+            alert('Se errar novamente, seu corpo será desovado na vila da folha.');
+            vidas++;
+            }
+
+        } else {
+            alert("Digite apenas A, B ou C")
+        }
+    }
+}
+
+
+
